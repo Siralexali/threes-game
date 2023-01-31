7c6f3ff5fa7bacf1ebf6427c390d6a5421d1bb0b
Siralexali <alisharafiyan1@gmail.com>
2023-01-31 19:14:31 +0330

    created right side of threes

--- threes.py
+++ threes.py
@@ -43,3 +43,39 @@ def randoml(mat):
     d = int(kd[1])
     mat[tmp[k % m]][-1] = d
     return mat
+
+
+def right(mat):
+    tedad = len(mat)
+    for i in range(tedad):
+        for j in range(-1, tedad, -1):
+            if mat[i][j] == 0:
+                (mat[i][j], mat[i][j-1]) = (mat[i][j-1], mat[i][j])
+    for i in range(tedad):
+        for j in range(-1, tedad, -1):
+            if mat[i][j] > 2 and (mat[i][j] == mat[i][j-1]):
+                mat[i][j] = mat[i][j] + mat[i][j-1]
+                mat[i][j-1] = 0
+                break
+            elif (mat[i][j] == 1 and mat[i][j-1] == 2) or (mat[i][j] == 2 and mat[i][j-1] == 1):
+                mat[i][j] = mat[i][j] + mat[i][j-1]
+                mat[i][j-1] = 0
+                break
+    return mat
+
+
+def randomr(mat):
+    m = 0
+    tmp = {}
+    key = []
+    for i in range(len(mat)):
+        if 0 == mat[i][0]:
+            m += 1
+            key = [i]
+    for i in range(m):
+        tmp[i] = key[i]
+    kd = input().split()
+    k = int(kd[0])
+    d = int(kd[1])
+    mat[tmp[k % m]][0] = d
+    return mat
