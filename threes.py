bf1d93545e2490d8686385f250ce5190e27c1776
Siralexali <alisharafiyan1@gmail.com>
2023-01-31 19:25:20 +0330

    created down side of threes

--- threes.py
+++ threes.py
@@ -87,8 +87,8 @@ def up(mat):
         for i in range(tedad-1):
             if mat[i][j] == 0:
                 (mat[i][j], mat[i+1][j]) = (mat[i+1][j], mat[i][j])
-    for i in range(tedad):
-        for j in range(tedad-1):
+    for j in range(tedad):
+        for i in range(tedad-1):
             if mat[i][j] > 2 and (mat[i][j] == mat[i+1][j]):
                 mat[i][j] = mat[i][j] + mat[i+1][j]
                 mat[i+1][j] = 0
@@ -114,4 +114,40 @@ def randomu(mat):
     k = int(kd[0])
     d = int(kd[1])
     mat[-1][tmp[k % m]] = d
+    return mat
+
+
+def down(mat):
+    tedad = len(mat)
+    for j in range(tedad):
+        for i in range(-1, tedad, -1):
+            if mat[i][j] == 0:
+                (mat[i][j], mat[i-1][j]) = (mat[i-1][j], mat[i][j])
+    for j in range(tedad):
+        for i in range(-1, tedad, -1):
+            if mat[i][j] > 2 and (mat[i][j] == mat[i-1][j]):
+                mat[i][j] = mat[i][j] + mat[i-1][j]
+                mat[i-1][j] = 0
+                break
+            elif (mat[i][j] == 1 and mat[i-1][j] == 2) or (mat[i][j] == 2 and mat[i-1][j] == 1):
+                mat[i][j] = mat[i][j] + mat[i-1][j]
+                mat[i-1][j] = 0
+                break
+    return mat
+
+
+def randomd(mat):
+    m = 0
+    tmp = {}
+    key = []
+    for i in range(len(mat)):
+        if 0 == mat[0][i]:
+            m += 1
+            key = [i]
+    for i in range(m):
+        tmp[i] = key[i]
+    kd = input().split()
+    k = int(kd[0])
+    d = int(kd[1])
+    mat[0][tmp[k % m]] = d
     return mat
